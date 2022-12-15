8d557c6fd3780f004819def90e8a133981b48f67
Agustin <agustinsilvafuentes@gmail.com>
2022-12-15 19:41:29 -0300

    mejoras a la entrega, faltaria un scrollbar y el filter, ya vi el tema de ejecución de app

---
+++ Entrega03/projectlist/webapp/controller/Detail.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent",
+    "sap/ui/core/routing/History",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, UIComponent, History, MessageToast, JSONModel) {
+    "use strict"
+    return Controller.extend("projectlist.controller.Detail", {
+        onInit: function () {
+
+        }
+    })
+})
